5b3d78fea727d6e8d3d221f953ba5e7c31beea93
Aditya Kharadkar <adityakharadkar27@gmail.com>
2025-03-30 11:54:37 +0530

    Initial commit

---
+++ data.js
@@ -0,0 +1,208 @@
+export const data = {
+  navLinks: [
+    { name: "Experience", id: "experience" },
+    { name: "Projects", id: "projects" },
+    { name: "Education", id: "education" },
+    { name: "Skills", id: "skills" },
+    { name: "Certificates", id: "certificates" },
+  ],
+  skills: [
+    {
+      name: "React",
+      type: "frontend",
+    },
+    {
+      name: "TypeScript",
+      type: "frontend",
+    },
+    {
+      name: "JavaScript",
+      type: "frontend",
+    },
+    {
+      name: "HTML",
+      type: "frontend",
+    },
+    {
+      name: "CSS",
+      type: "frontend",
+    },
+    {
+      name: "Node.js",
+      type: "backend",
+    },
+    {
+      name: "Python",
+      type: "backend",
+    },
+    {
+      name: "Git",
+      type: "version-control",
+    },
+    {
+      name: "Material UI",
+      type: "frontend-library",
+    },
+    {
+      name: "Tailwind CSS",
+      type: "css-framework",
+    },
+    {
+      name: "Bootstrap",
+      type: "css-framework",
+    },
+  ],
+  certificates: [
+    {
+      courseName: "Namaste React",
+      courseLink:
+        "https://namastedev.com/adityakharadkar27/certificates/namaste-react",
+      vendor: "NamasteDev.com",
+    },
+    {
+      courseName: "Frontend Engineer",
+      courseLink:
+        "https://namastedev.com/adityakharadkar27/certificates/namaste-react",
+      vendor: "Codecademy",
+    },
+    {
+      courseName: "Frontend Developer",
+      courseLink:
+        "https://namastedev.com/adityakharadkar27/certificates/namaste-react",
+      vendor: "Hackerrank",
+    },
+    {
+      courseName: "Understanding TypeScript",
+      courseLink:
+        "https://namastedev.com/adityakharadkar27/certificates/namaste-react",
+      vendor: "Udemy",
+    },
+    {
+      courseName: "React - The Complete Guide",
+      courseLink:
+        "https://namastedev.com/adityakharadkar27/certificates/namaste-react",
+      vendor: "Udemy",
+    },
+  ],
+  education: [
+    {
+      universityName:
+        "Dr. Babasaheb Ambedkar Marathwada University, Aurangabad",
+      degreeName: "Bachelor in Computer Science",
+      start: "Jun 2018",
+      end: "Apr 2021",
+      percentage: "77.57%",
+    },
+    {
+      universityName:
+        "Dr. Babasaheb Ambedkar Marathwada University, Aurangabad",
+      degreeName: "Master in Computer Science",
+      start: "Jun 2021",
+      end: "Apr 2023",
+      percentage: "84.50%",
+    },
+  ],
+  experience: [
+    {
+      companyName: "Tata Consultancy Services",
+      location: "Pune, Maharashtra",
+      position: "Frontend Developer",
+      duration: "Aug 2021 - Present",
+      projects: [
+        {
+          name: "In-House Application",
+          duration: "Oct 2024 - Present",
+          description: [
+            "Develop and enhance a modular in-house application that integrates multiple web apps using React, TypeScript, JavaScript, and Material UI.",
+            "Leverage the Olympe visula programming platform to create and optimize workflows within the application.",
+            "Collaborate with backend teams to seamlessly integrate APIs and ensure efficient data handling.",
+            "Focus on building reusable components and optimizing frontend performance for a consistent user experience.",
+            "Debug, troubleshoot and resolve frontend issues to maintain application stability and performance.",
+            "Utilize Git for version control, ensuring smooth collaboration through proper branching and merging strategies.",
+          ],
+        },
+        {
+          name: "E-commerce Retail Platform",
+          duration: "Sep 2021 - Sep 2024",
+          description: [
+            "Developed a dynamic, user friendly, and responsive UI using React.js, Redux, TypeScript, JavaScript, and Material UI.",
+            "Implemented brand-based search filters to enhance product discoverability and improve user experience.",
+            "Designed and integrated a shopping cart system requiring users to accept agreements before submitting orders, ensuring compliance.",
+            "Enhanced the order table to display user-submitted orders efficiently with clear data presentation.",
+            "Contributed to middleware development using Node.js to map fronted API calls to backend endpoints.",
+            "Coordinated with backend developers to integrate RESTful APIs for seamless data flow.",
+            "Utilized Git for version control, implementing best practices for efficient collaboration and code management.",
+          ],
+        },
+      ],
+    },
+  ],
+  projects: [
+    {
+      name: "Web Scraper - A Job Posting Application",
+      duration: "Apr 2023 - May 2023",
+      description:
+        "Developed a job listing app that provides real-time job postings from multiple sources.",
+      tags: ["React.js", "Node.js", "Python", "Web Scraping"],
+      link: "https://github.com/adikharadkar/WebScraper--MERN-Python",
+    },
+    {
+      name: "Burger Ordering Application",
+      duration: "Sep 2020 - Nov 2020",
+      description:
+        "Developed a web app for ordering custom burgers with user authentication and order management.",
+      tags: ["React.js", "Redux", "Firebase"],
+      link: "https://github.com/adikharadkar/Burger-Builder-Web-App",
+    },
+    {
+      name: "Web Scraper - Ubuntu Security Notices Page",
+      duration: "Aug 2022",
+      description:
+        "Designed and developed a web scraper that scrapes Ubuntu's Security Notices webpage.",
+      tags: ["Python", "Web Scraping"],
+      link: "https://github.com/adikharadkar/Python-Web-Scraper",
+    },
+    {
+      name: "Airline Ticket Booking App",
+      duration: "Jun 2021",
+      description:
+        "Developed an application to book a flight ticket, view the booking history, create user account, login and logout.",
+      tags: ["Python", "HTML", "CSS", "JavaScript", "MySQL"],
+      link: "https://github.com/adikharadkar/Airline-Ticket-Booking",
+    },
+    {
+      name: "Insurance Portal",
+      duration: "Jun 2021",
+      description:
+        "Developed an application where users can create their account, login, select a policy, view the policy history and logout.",
+      tags: ["Python", "HTML", "CSS", "JavaScript", "MySQL"],
+      link: "https://github.com/adikharadkar/Insurance-Portal",
+    },
+    {
+      name: "Bookstore App",
+      duration: "Jan 2021",
+      description:
+        "Developed a desktop application where users can view all the books, add new books, update or delete an existing book, etc.",
+      tags: ["Python", "Tkinter", "Sqlite3"],
+      link: "https://github.com/adikharadkar/Bookstore-App",
+    },
+  ],
+  profiles: [
+    {
+      name: "Email",
+      link: "mailto:adityakharadkar27gmail.com",
+    },
+    {
+      name: "LinkedIn",
+      link: "https://www.linkedin.com/in/aditya-kharadkar-6352ba174/",
+    },
+    {
+      name: "Github",
+      link: "https://github.com/adikharadkar/",
+    },
+    {
+      name: "Leetcode",
+      link: "https://leetcode.com/u/adikharadkar/",
+    },
+  ],
+};
---
+++ index.js
@@ -0,0 +1,241 @@
+import { data } from "./data.js";
+
+const skillList = document.getElementById("skill-list");
+
+if (!skillList) {
+  console.error("Skill list element not found!");
+} else {
+  data.skills.forEach((s) => {
+    const li = document.createElement("li");
+    li.textContent = s.name;
+    li.setAttribute("id", "skill");
+    skillList.appendChild(li);
+  });
+}
+
+// Nav Links
+const navLinks = document.getElementById("nav-links");
+
+if (navLinks) {
+  const ul = document.createElement("ul");
+  navLinks.appendChild(ul);
+  ul.classList.add("nav-ul");
+  ul.setAttribute("id", "nav-links-ul");
+
+  data.navLinks.forEach((el) => {
+    const li = document.createElement("li");
+    li.setAttribute("id", "nav-link");
+    li.classList.add("nav-links");
+    li.textContent = el.name;
+
+    li.addEventListener("click", () => {
+      window.location.hash = el.id;
+    });
+    ul.appendChild(li);
+  });
+}
+
+function handleMobileNavbar() {
+  const mobileNavbar = document.getElementById("mobile-navbar");
+  if (mobileNavbar.classList.contains("show-mobile-nav")) {
+    mobileNavbar.classList.remove("show-mobile-nav");
+    mobileNavbar.style = "display: none";
+  } else {
+    mobileNavbar.classList.add("show-mobile-nav");
+    mobileNavbar.style = "display: contents";
+  }
+}
+
+const navMenuIcon = document.getElementById("nav-menu-icon");
+if (navMenuIcon) {
+  navMenuIcon.addEventListener("click", handleMobileNavbar);
+}
+
+const mobileNavbar = document.getElementById("mobile-navbar");
+if (mobileNavbar) {
+  const mobileNavList = document.createElement("ul");
+  mobileNavList.classList.add("mobile-nav-list");
+  data.navLinks.forEach((navLink) => {
+    const mobileNavLink = document.createElement("li");
+    mobileNavLink.classList.add("mobile-nav-link");
+    mobileNavLink.textContent = navLink.name;
+    mobileNavLink.addEventListener("click", () => {
+      window.location.hash = navLink.id;
+    });
+    mobileNavList.appendChild(mobileNavLink);
+  });
+  mobileNavbar.appendChild(mobileNavList);
+}
+
+// Certificates
+const certificateList = document.getElementById("certificate-list");
+if (certificateList) {
+  data.certificates.forEach((el) => {
+    const li = document.createElement("li");
+    li.classList.add("certificate");
+    const a = document.createElement("a");
+    a.classList.add("certificate-link");
+    a.textContent = `${el.courseName} - ${el.vendor}`;
+    a.href = el.courseLink;
+    a.target = "_blank";
+    li.appendChild(a);
+    certificateList.appendChild(li);
+  });
+}
+
+// Education
+const educationList = document.getElementById("education-list");
+if (educationList) {
+  data.education.forEach((el) => {
+    const li = document.createElement("li");
+    li.classList.add("list-item", "education-list-item");
+
+    const degreeName = document.createElement("h3");
+    degreeName.textContent = el.degreeName;
+
+    const paraDurationDiv = document.createElement("div");
+    paraDurationDiv.classList.add("para-duration-div");
+
+    const duration = document.createElement("p");
+    duration.textContent = `${el.start} - ${el.end}`;
+
+    const universityName = document.createElement("h4");
+    universityName.textContent = el.universityName;
+
+    const percentage = document.createElement("span");
+    percentage.textContent = el.percentage;
+
+    paraDurationDiv.appendChild(degreeName);
+    paraDurationDiv.appendChild(duration);
+    li.appendChild(paraDurationDiv);
+    li.appendChild(universityName);
+    li.appendChild(percentage);
+    educationList.appendChild(li);
+  });
+}
+
+const experienceList = document.getElementById("experience-list");
+
+if (experienceList) {
+  data.experience.forEach((el) => {
+    const li = document.createElement("li");
+    li.classList.add("list-item");
+
+    const position = document.createElement("h2");
+    position.textContent = el.position;
+    li.appendChild(position);
+
+    const h3 = document.createElement("h3");
+    h3.textContent = el.companyName;
+    li.appendChild(h3);
+
+    const durationLocationDiv = document.createElement("section");
+    durationLocationDiv.setAttribute("id", "durationLocationDiv");
+
+    const durationSpan = document.createElement("span");
+    durationSpan.textContent = el.duration;
+
+    const locationSpan = document.createElement("span");
+    locationSpan.textContent = el.location;
+
+    li.appendChild(durationLocationDiv);
+    durationLocationDiv.appendChild(durationSpan);
+    durationLocationDiv.appendChild(locationSpan);
+    durationLocationDiv.classList.add("durationLocationDiv");
+
+    const projectList = document.createElement("ul");
+
+    el.projects.forEach((pro) => {
+      const project = document.createElement("li");
+      const projectName = document.createElement("h4");
+      projectName.textContent = pro.name + " (" + pro.duration + ")";
+      project.appendChild(projectName);
+      projectList.appendChild(project);
+
+      const descriptionList = document.createElement("ul");
+      project.appendChild(descriptionList);
+
+      pro.description.forEach((desc) => {
+        const descLi = document.createElement("li");
+        descLi.textContent = desc;
+        descriptionList.appendChild(descLi);
+      });
+    });
+
+    li.appendChild(projectList);
+
+    experienceList.appendChild(li);
+  });
+}
+
+const projectsList = document.getElementById("projects-list");
+data.projects.forEach((pro) => {
+  const projectListItem = document.createElement("li");
+  projectListItem.classList.add("projectListItem");
+
+  const projectNameDurationDiv = document.createElement("div");
+
+  const projectName = document.createElement("h3");
+  projectName.textContent = pro.name;
+  projectName.classList.add("projectName");
+  projectNameDurationDiv.appendChild(projectName);
+
+  const projectDuration = document.createElement("span");
+  projectDuration.textContent = pro.duration;
+
+  projectNameDurationDiv.classList.add("projectNameDurationDiv");
+
+  const projectDescription = document.createElement("p");
+  projectDescription.textContent = pro.description;
+
+  const projectTags = document.createElement("ul");
+  projectTags.classList.add("projectTags");
+  pro.tags.forEach((tag) => {
+    const projectTagItem = document.createElement("li");
+    projectTagItem.textContent = tag;
+    projectTagItem.classList.add("projectTagItem");
+    projectTags.appendChild(projectTagItem);
+  });
+
+  const githubButton = document.createElement("a");
+  githubButton.textContent = "View on Github";
+  githubButton.href = pro.link;
+  githubButton.setAttribute("target", "_blank");
+  githubButton.classList.add("githubButton");
+
+  projectListItem.appendChild(projectNameDurationDiv);
+  projectListItem.appendChild(projectDuration);
+  projectListItem.appendChild(projectDescription);
+  projectListItem.appendChild(projectTags);
+  projectNameDurationDiv.appendChild(githubButton);
+  projectsList.appendChild(projectListItem);
+});
+
+const sideNav = document.getElementById("side-nav");
+const profilesList = document.createElement("ul");
+profilesList.classList.add("profiles-list");
+data.profiles.forEach((profile) => {
+  const profileListItem = document.createElement("li");
+  profileListItem.setAttribute("id", "profile-list-item");
+  profileListItem.classList.add("profile-list-item");
+
+  const profileLink = document.createElement("a");
+  profileLink.classList.add("profile-link");
+  profileLink.textContent = profile.name;
+  profileLink.href = profile.link;
+  profileLink.setAttribute("target", "_blank");
+
+  profileListItem.appendChild(profileLink);
+  profilesList.appendChild(profileListItem);
+});
+const downloadLink = document.createElement("a");
+const downloadListItem = document.createElement("li");
+downloadListItem.appendChild(downloadLink);
+downloadLink.textContent = "Download Resume";
+downloadListItem.classList.add("profile-list-item");
+downloadLink.classList.add("profile-link");
+downloadLink.href =
+  "Aditya Kharadkar_Frontend Developer_3 Years 6 Months_7028281198.pdf";
+downloadLink.setAttribute("download", "aditya_kharadkar.pdf");
+profilesList.appendChild(downloadListItem);
+sideNav.appendChild(profilesList);
